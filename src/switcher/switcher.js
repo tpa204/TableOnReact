42b824d1908179548da1190823a01f712c7a3e58
Tarasov Joseph Pavel <tpa204@gmail.com>
2022-02-14 22:44:09 +0200

    new branch

--- src/switcher/switcher.js
+++ src/switcher/switcher.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react';
+import   "./switcher.css"
 
 const Switcher = ({buttonHandler}) => {
     const url="http://www.filltext.com/?rows=16&id={index}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
     const URL="http://www.filltext.com/?rows=200&id={index}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
     return (
-      <Fragment>
-        <button onClick={() => {buttonHandler(URL)}}  className='btn btn-danger'>200rows</button>
-        <button onClick={() => {buttonHandler(url)}}  className='btn btn-warning'>16rows</button>
+      <Fragment >
+        <div className='containerrr'>
+        <button  onClick={() => {buttonHandler(URL)}}  className='btn btn-danger btns'>200rows</button>
+        <button onClick={() => {buttonHandler(url)}}  className='btn btn-warning btns'>16rows</button>
+        </div>
+        
       </Fragment>
     );
 };
