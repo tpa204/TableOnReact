38f49170543280dbfec30cc1330f6a3a5db5a996
Tarasov Joseph Pavel <tpa204@gmail.com>
2022-03-15 20:55:42 +0200

    paginator later2

--- src/paginator/paginator.js
+++ src/paginator/paginator.js
@@ -5,16 +5,16 @@ const Paginator = ({pages,currentPage,onPrevClick,onNextClick}) => {
         <nav aria-label="...">
         <ul className="pagination">
           <li className="page-item disabled">
-            <a className="page-link" href="#" tabIndex="-1" onClick={()=>{onPrevClick()}}>Previous</a>
+            <a className="page-link" href="/some/valid/uri" tabIndex="-1" onClick={()=>{onPrevClick()}}>Previous</a>
           </li>
          {pages.map(p=>{
              return (
-                <li className="page-item" key = {p}><a className="page-link" onClick={()=>{currentPage(p)}} href="#">{p}</a></li>
+                <li className="page-item" key = {p}><a className="page-link" onClick={()=>{currentPage(p)}} href="/some/valid/uri">{p}</a></li>
              )
          })}
           <li className="page-item">
          
-      <a className="page-link" href="#" onClick={()=>{onNextClick()}}>Next</a>
+      <a className="page-link" href="/some/valid/uri" onClick={()=>{onNextClick()}}>Next</a>
     </li>
         </ul>
       </nav>
